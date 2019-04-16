4fbb3a1a22b5204bef36b11689dbc12119fb9fa0
SataQiu <qiushida@beyondcent.com>
2019-04-17 01:32:01 +0800

    framework init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # redis-operator
-A redis-operator based on operator-sdk.
+A redis-operator based on operator-sdk (operator-sdk version v0.5.0+git)
+
---
+++ pkg/apis/redis/v1alpha1/redis_types.go
@@ -0,0 +1,49 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// RedisSpec defines the desired state of Redis
+// +k8s:openapi-gen=true
+type RedisSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// RedisStatus defines the observed state of Redis
+// +k8s:openapi-gen=true
+type RedisStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Redis is the Schema for the redis API
+// +k8s:openapi-gen=true
+type Redis struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RedisSpec   `json:"spec,omitempty"`
+	Status RedisStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// RedisList contains a list of Redis
+type RedisList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Redis `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Redis{}, &RedisList{})
+}
